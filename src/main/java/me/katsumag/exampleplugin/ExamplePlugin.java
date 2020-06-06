7eaeb1da6124e152a8a1f446e70ae4fdf98b8802
katsumag <goude47@gmail.com>
2020-06-06 11:23:51 +0100

    Initial commit

---
+++ src/main/java/me/katsumag/exampleplugin/ExamplePlugin.java
@@ -0,0 +1,17 @@
+package me.katsumag.exampleplugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ExamplePlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
